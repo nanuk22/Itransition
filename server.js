c7adcda655ed3972eebc12fae925f60e777495a9
nanuk22 <nanka-222@yandex.ru>
2024-03-08 22:10:59 +0300

    added extra src

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Serve static files from the 'client/build' directory
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'src/client/build')));
 
 // Define your API routes or other server logic here
 
 // Send the React app for any other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'src/client/build', 'index.html'));
 });
 
 // Start the server on the specified port
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
