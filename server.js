7071374cfc34080600191e32286075a8e6b56038
nanuk22 <nanka-222@yandex.ru>
2024-03-28 23:40:00 +0300

    added correct pathes in server.js for build

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.get('/test', async (req, res) => {
 
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
